0c2b7bf83cc06661043954397e893f9497fe4ab1
TheCodingRocket <73431748+TheCodingRocket@users.noreply.github.com>
2020-11-24 09:45:02 +0700

    Create swing.js

---
+++ swing.js
@@ -0,0 +1,28 @@
+class Mover {
+  constructor(x, y, m) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(5);
+    this.acc = createVector(0, 0);
+    this.mass = m;
+    this.r = sqrt(this.mass) * 2;
+  }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255, 100);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
